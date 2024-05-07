7b46798aacc44a0a059a44306ad1de717c8b81cc
Antonio <antoniominafra.dev@gmail.com>
2024-05-07 17:03:07 +0200

    aggiunta img user e gestita nel frontend

--- resources/views/livewire/index-chirp.blade.php
+++ resources/views/livewire/index-chirp.blade.php
@@ -7,17 +7,17 @@
             <div class="col-12 ">
                 <div class="tweet-wrap">
                     <div class="tweet-header">
-                        <img src="https://pbs.twimg.com/profile_images/1012717264108318722/9lP-d2yM_400x400.jpg" alt="" class="avator">
+                        <img src={{$chirp->user->img}} alt="" class="avator">
                         <div class="tweet-header-info">
                             {{$chirp->user->name}}
                             <p>🔥{{$chirp->content}}</p>
                         </div>
                     </div>
-                        {{-- il dato tag arriva direttamente --}}
+                    {{-- il dato tag arriva direttamente --}}
                     @if($chirp->tags->isNotEmpty())
                     <div class="mb-3 ">
                         @foreach($chirp->tags as $tag)
-                            <span class="badge text-bg-primary">#{{$tag->name}}</span>
+                        <span class="badge text-bg-primary">#{{$tag->name}}</span>
                         @endforeach
                     </div>
                     @endif
@@ -26,12 +26,15 @@
                         <img src="{{Storage::url($chirp->img)}}" alt="" class="tweet-img">
                     </div>
                     <div class="mx-auto text-end my-3 w-100">
+                        {{-- controllo se sei il creatore dell articolo vedo i bottoni del modifica ed elimina--}}
+                        @if (Auth::id() == $chirp->user->id)
                         <a href="{{route('edit', compact('chirp'))}}">
                             <i class="bi text-primary fs-4 bi-pencil-square"></i>
                         </a>
                         <button class="btn" wire:click="destroy({{$chirp}})">
                             <i class="bi text-danger  fs-4 bi-trash3"></i>
                         </button>
+                        @endif
                     </div>
 
                 </div>
